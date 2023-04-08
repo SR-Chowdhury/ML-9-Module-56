5b6cbe1df69a7b2ffc7954a348469944cd7c46f3
Shihan Chowdhury <hackedgdown@gmail.com>
2023-04-08 18:06:24 +0100

    ML-9-Module 56-3 done

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 
-const Cart = () => {
+const Cart = ({cart, handleRemoveFromCart}) => {
     return (
         <div>
-            <h1>Order Summary</h1>  
+            <h3>Order Summary: {cart.length}</h3>
+            <ol>
+            {
+                cart.map(item => <li>
+                    {item.name}
+                    <button onClick={() => handleRemoveFromCart(item._id)} className='ms-3'>X</button>
+                </li>)
+            }  
+            </ol>
         </div>
     );
 };
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,6 +3,8 @@ import { useLoaderData } from 'react-router-dom';
 import TShirt from '../TShirt/TShirt';
 import Cart from '../Cart/Cart';
 import './Home.css';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Home = () => {
 
@@ -10,23 +12,54 @@ const Home = () => {
 
     const [cart, setCart] = useState([]);
 
-    const handleAddToCart = tshirts => console.log(tshirts);
+    const notify = () => toast("You've already added this item!");
 
-    
+    const handleAddToCart = tshirt => {
+        const exists = cart.find(item => item === tshirt);
+        if (exists) {
+            notify();
+        }
+        else {
+            const newCart = [...cart, tshirt];
+            setCart(newCart)
+        }
+    };
+
+    const handleRemoveFromCart = id => {
+        const remainingItem = cart.filter(item => item._id !== id);
+        setCart(remainingItem);
+    }
+
+
     return (
         <div className='homeContainer'>
             <div className="tshirstContainer">
                 {
-                    tshirts.map(tshirt => <TShirt 
-                        key={tshirt._id} 
-                        tshirt = {tshirt}
-                        handleAddToCart = {handleAddToCart}
+                    tshirts.map(tshirt => <TShirt
+                        key={tshirt._id}
+                        tshirt={tshirt}
+                        handleAddToCart={handleAddToCart}
                     />)
                 }
             </div>
             <div className="cartContainer">
-                <Cart/>
+                <Cart
+                    cart={cart}
+                    handleRemoveFromCart={handleRemoveFromCart}
+                />
             </div>
+            <ToastContainer
+                position="top-center"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="light"
+            />
         </div>
     );
 };
